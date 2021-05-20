e4555da580c0594e24bfde04f69c80ab55295aa9
econc017 <econc017@fiu.com>
2021-05-19 22:17:52 -0400

    architecting

--- Python/OneDayStockProjection/mlStockPrice_oneDayProjection.py
+++ Python/OneDayStockProjection/mlStockPrice_oneDayProjection.py
@@ -2,7 +2,15 @@ import pandas
 import pandas_datareader as dataReader
 import numpy
 import matplotlib.pyplot as plot
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
+
+
+def loadData(company: str, startDate: str):
+    company = company
 
 
 if __name__ == "__main__":
-    print("hi")
+    loadData("TTCF", 123)
