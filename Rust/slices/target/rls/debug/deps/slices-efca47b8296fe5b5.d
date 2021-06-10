cd2b4a8898d88ba0926339679ba735132d34c7cd
econc017 <econc017@fiu.com>
2021-06-09 22:17:44 -0400

    slices from book exmaples

---
+++ Rust/slices/target/rls/debug/deps/slices-efca47b8296fe5b5.d
@@ -0,0 +1,5 @@
+c:\Users\edgar\Documents\github\projects\Rust\slices\target\rls\debug\deps\slices-efca47b8296fe5b5.rmeta: src\main.rs
+
+c:\Users\edgar\Documents\github\projects\Rust\slices\target\rls\debug\deps\slices-efca47b8296fe5b5.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/slices/target/rls/debug/deps/slices-f10bd7da08e1eb9c.d
@@ -0,0 +1,5 @@
+c:\Users\edgar\Documents\github\projects\Rust\slices\target\rls\debug\deps\slices-f10bd7da08e1eb9c.rmeta: src\main.rs
+
+c:\Users\edgar\Documents\github\projects\Rust\slices\target\rls\debug\deps\slices-f10bd7da08e1eb9c.d: src\main.rs
+
+src\main.rs:
